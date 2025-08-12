a9d1b45b436a16336cc42037f4c46151917638b3
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2025-08-12 19:45:39 +0000

    Aula 12/08/2025

---
+++ src/main/java/application/model/Tarefa.java
@@ -0,0 +1,15 @@
+package application.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Tarefa {
+    @Id
+    private long id;
+    private String descricao;
+}
